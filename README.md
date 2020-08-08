e5afbf8df2c2f7dfed863a9bb4cbd4250e8b0242
Pedro Araujo <pedroaraujonogueirati@gmail.com>
2020-08-07 21:28:02 -0300

    Projeto ChatBot Telegram.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Projeto ChatBot Telegram
+
+## Como executar o projeto: 
+
+-> Crie seu chatbot do telegram e use o **token** gerado no arquivo **.env** do projeto.
+-> Dê um **npm install** para baixar todas as dependências do projeto. 
+-> Para utilizar o Bot em uma conversa individual basta escrever **/start** e em seguida digitar os nomes dos produtos que deseja adicionar na lista.
+-> Para Utilizar o Bot em grupo é necessário adicioná-lo a algum grupo e pra adicionar itens na lista é necessário escrever **/nomeDoProduto**, por exemplo: para adicionar arroz escreva **/arroz**.
+
+## Descrição do projeto:
+
+-> O projeto é um chatbot, feito em Node, para o telegram no qual a partir desse chatbot é possível fazer uma lista de compras, adicionando e removendo produtos de acordo com a necessidade. O chatbot pode ser adicionado a um grupo ou ser usado em uma conversa individual.
+
+## Requisitos de Softwares e Bibliotecas:
+
+-> Node.
+-> npm/yarn.
+-> Bibliotecas do Telegraf (usado para fazer a comunicação com a API do telegram).
+
+## Links Úteis:
+
+API telegram: <https://core.telegram.org/bots/api>.
+API telegraf: <https://telegraf.js.org/#/>. 
+Github do telegraf: <https://github.com/telegraf/telegraf>.
+Introdução ao chatBot <https://core.telegram.org/bots>.
---
+++ src/listaComprasGrupo.js
@@ -0,0 +1,50 @@
+const env = require('../../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const bot = new Telegraf(env.token)
+
+let dados = {}
+
+const botoes = lista => Extra.markup(
+    Markup.inlineKeyboard(
+        lista.map(item => Markup.callbackButton(item, `delete ${item}`))
+            , {columns: 3}
+    )
+)
+
+bot.start(async ctx => {
+    const name = ctx.update.message.from.first_name
+    await ctx.reply(`Seja bem vindo, ${name}!`)
+    await ctx.reply('Escreva os itens que você deseja adicionar.!.!.!')
+})
+
+bot.use((ctx, next) => {
+    const chatId = ctx.chat.id
+    if(!dados.hasOwnProperty(chatId)){ // O método hasOwnProperty() retorna um booleano indicando se o objeto possui a propriedade especificada como uma propriedade definida no próprio objeto em questão (ao contrário de uma propriedade herdada).
+        dados[chatId] = []
+    }
+    ctx.itens = dados[chatId]
+    next()
+})
+
+bot.on('text', ctx => {
+    console.log(ctx)
+    let texto = ctx.message.text
+    //  O método startsWith() determina se uma string começa com os caracteres da string especificada, retornando true ou false
+    if(texto.startsWith('/')){
+        texto = texto.substring(1)
+    }
+    ctx.itens.push(texto)
+    ctx.reply(`${texto} adicionado!`, botoes(ctx.itens))
+})
+
+bot.action(/delete (.+)/, ctx => {
+    const indice = ctx.itens.indexOf(ctx.match[1])
+    if(indice >= 0 ){
+        ctx.itens.splice(indice, 1)  
+    }
+    ctx.reply(`${ctx.match[1]} deletado`, botoes(ctx.itens))
+})
+
+bot.startPolling()
